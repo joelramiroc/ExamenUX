2fff3763823be2b6b50143e90019f2b52ac183f3
joelramiroc <www.joelramiro@unitec.edu>
2018-08-14 20:09:04 -0600

    changes

--- src/Weather.js
+++ src/Weather.js
@@ -12,26 +12,26 @@ class JSon extends Component {
     }
 
     render() {
-        var result = 'http://api.openweathermap.org/data/2.5/weather?q=Yoro,Hn&appid=851d5b33569b7c1a2e15edb5270db02d&units=metric';
+        var resulta = 'http://api.openweathermap.org/data/2.5/weather?q='+this.state.city+','+this.state.country+'&appid=851d5b33569b7c1a2e15edb5270db02d&units=metric';
 
         return (
             <ul className = "ulList">
                 <li>
                     <div>
-                        {/* <h2>CIUDAD</h2> */}
-                        <h2>{result.name}</h2>
+                        <h2>CIUDAD</h2>
+                        <h2>{resulta.name}</h2>
                     </div>
                 </li>
                 <li>
                     <div>
-                        {/* <h2>TEMPERATURA</h2> */}
-                        <h2>{result.temp}</h2>
+                        <h2>TEMPERATURA</h2>
+                        <h2>{resulta.temp}</h2>
                     </div>
                 </li>
                 <li>
                     <div>
-                        {/* <h2>HUMEDAD</h2> */}
-                        <h2>{result.humidity}</h2>
+                        <h2>HUMEDAD</h2>
+                        <h2>{resulta.humidity}</h2>
                     </div>
                 </li>
             </ul>
